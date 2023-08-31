24972ca5b16e60d3e89a06d4f16d934f2100a1d1
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-08-31 15:55:14 +0100

    Add const variables for button elements and the value of 'document.getElementById' for each one

---
+++ app.js
@@ -0,0 +1,14 @@
+// For first container 
+const onButton = document.getElementById('big-button');
+const enterButton = document.getElementById('blue-button');
+const upButton = document.getElementById('up-button');
+const leftButton = document.getElementById('left-button');
+const rightButton = document.getElementById('right-button');
+const downButton = document.getElementById('down-button');
+
+// For second container
+const numberButton = documnet.getElementById('dex-buttons');
+const navigateLeft = document.getElementById('nav-left');
+const navigateRight = document.getElementById('nav-right');
+
+onButton.addEventListener('click', turnOn)
