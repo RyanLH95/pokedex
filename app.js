ce6148d3f211043c72e94038d5405788a0cc3a63
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-08-31 16:45:41 +0100

    Add var variables for information about pokemon e.g. name, height, weight etc and make value 'document.getElementById(). Add function for 'directional buttons'.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // For first container 
+// buttons
 const onButton = document.getElementById('big-button');
 const enterButton = document.getElementById('blue-button');
 const upButton = document.getElementById('up-button');
@@ -11,4 +12,41 @@ const numberButton = documnet.getElementById('dex-buttons');
 const navigateLeft = document.getElementById('nav-left');
 const navigateRight = document.getElementById('nav-right');
 
+var name = document.getElementById('name')
+var height = document.getElementById('height')
+var weight = document.getElementById('weight')
+var description = document.getElementById('description')
+var primaryType = document.getElementById('primary-type')
+var secondaryType = document.getElementById('secondary-type')
+
 onButton.addEventListener('click', turnOn)
+
+enterButton.addEventListener('click', () => {
+    showPokemon()
+}) 
+
+upButton.addEventListener('click', () => {
+    goUpOne++
+    showPokemon()
+})
+
+leftButton.addEventListener('click', () => {
+    goUpTen++
+    showPokemon()
+})
+
+downButton.addEventListener('click', () => {
+    goDownOne++
+    showPokemon()
+})
+
+rightButton.addEventListener('click', () => {
+    goDownTen++
+    showPokemon()
+})
+
+const pokemon = [
+    {
+        
+    }
+]
